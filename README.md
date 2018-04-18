9b0e71b02f7b477837268540abb6bd6cc90fb0ea
jeromesourdin <jerome.sourdin@cotesdarmor.fr>
2018-04-18 18:20:16 +0200

    ok

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ formation
 =========
 
 A Symfony project created on April 16, 2018, 1:34 pm.
+
+https://blog.netapsys.fr/open-street-map-partie-2-integration-dune-carte-avec-leaflet/
+https://wiki.openstreetmap.org/wiki/Tile_servers
+https://adresse.data.gouv.fr/api
+http://docs.guzzlephp.org/en/stable/index.html
+
+
+http://www.odata.org/documentation/odata-version-2-0/uri-conventions/
+http://datarmor.cotesdarmor.fr/portail-donnees/#/environments/cg22/api
+
+
+
--- src/AppBundle/Controller/ProduitsController.php
+++ src/AppBundle/Controller/ProduitsController.php
@@ -15,6 +15,7 @@ use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\HttpFoundation\Request;
+use GuzzleHttp\Client;
 
 class ProduitsController extends Controller
 {
@@ -51,6 +52,8 @@ class ProduitsController extends Controller
      */
     public function ajoutAction(Request $request, $code)
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'Po le droit !');
+
         if (is_null($code))
         {
             $produit = new Produits();
@@ -66,6 +69,7 @@ class ProduitsController extends Controller
                 );
             }
         }
+
 
         $form = $this->createFormBuilder($produit)
             ->add('code', TextType::class)
@@ -115,5 +119,60 @@ class ProduitsController extends Controller
         return $this->render('@App/default/produitSupp.html.twig', array(
             'produit' => $code
         ));
+    }
+
+    /**
+     * @Route("/api", name="api")
+     */
+    public function apiAction()
+    {
+        $client = new Client([
+            // Base URI is used with relative requests
+            'base_uri' => 'https://api-adresse.data.gouv.fr/api',
+            // You can set any number of default request options.
+            'timeout'  => 2.0,
+        ]);
+
+        $data = $client->request(
+                'GET', 
+                'search/?q=13 bd Carnt&postcode=22000',
+                [
+                    'stream' => true,
+                    'stream_context' => ['ssl' => ['allow_self_signed' => true]]
+                ]
+            )
+            ->getBody()
+            ->getContents();
+        $data = json_decode($data);
+
+        $client2 = new Client([
+            // Base URI is used with relative requests
+            'base_uri' => 'http://datarmor.cotesdarmor.fr/dataserver/cg22/data/',
+            // You can set any number of default request options.
+            'timeout'  => 2.0,
+        ]);
+
+        $data2 = $client2->request(
+                'GET', 
+                'i3ikmnzn?$inlinecount=allpages&$select=RSANA&$format=json&$filter=AN_PREL gt 2007 and substringof(tolower('. chr(39) .'417300'. chr(39) .'),tolower(CDSTATIONMESUREEAUXSURFACE)) and NOMPARAMETRE eq '. chr(39) .'Nitrates'. chr(39) .'&$top=1000&$skip=0',
+                [
+                    'stream' => true,
+                    'stream_context' => ['ssl' => ['allow_self_signed' => true]]
+                ]
+            )
+            ->getBody()
+            ->getContents();
+        $data2 = json_decode($data2);
+
+
+
+        return $this->render(
+            '@App/default/api.html.twig', 
+            array(
+                'data' => $data->features[0]->geometry->coordinates,
+                'data2' => $data2
+            )
+        );
+
     }
 }
